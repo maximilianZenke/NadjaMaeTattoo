18867e534471e5276009528ac8d7e4717a665699
mzenke <max.zenke@me.com>
2026-04-26 17:03:57 +0200

    improve component styling

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,72 +4,84 @@ import ueberMich from "./ueberMich.png";
 import frau from "./nadja.png";
 
 export default function AboutMe() {
-  return (
-    <>
-      <div id="about" />
-      <div className="aboutme-wrapper">
-        <div className="aboutme-container row align-items-center">
-          <div className="col-md-6 aboutme-text">
-            <img
-              src={ueberMich}
-              alt="Über mich"
-              className="aboutme-heading"
-              loading="lazy"
-            />
+    return (
+        <>
+            <div id="about" />
+            <div className="aboutme-wrapper">
+                <div className="aboutme-container">
 
-            <div className="aboutme-text-content">
-              <p>
-                Hi, ich bin Nadja, Tätowiererin aus Tübingen.
-              </p>
+                    <img
+                        src={ueberMich}
+                        alt="Über mich"
+                        className="aboutme-heading"
+                        loading="lazy"
+                    />
 
-              <p>
-                Meine Liebe für's Tätowieren habe ich schon sehr früh entdeckt
-                und seit ich 14 Jahre alt bin beschäftige ich mich intensiv damit.
-                Seit ca. 12 Jahren arbeite ich nun schon selbstständig in dem
-                Beruf und versuche mich stetig weiterzuentwickeln und meinen Stil
-                zu verfeinern.
-              </p>
+                    <div className="aboutme-top">
+                        <div className="aboutme-text">
+                            <p className="aboutme-greeting">
+                                Hi,<br />
+                                ich bin<br />
+                                <span>Nadja.</span>
+                            </p>
+                        </div>
 
-              <p>
-                Ich habe als Allrounderin angefangen, mich inzwischen aber vor
-                allem auf „Neotraditional“ spezialisiert. Dieser Stil vereint die
-                klare Linienführung des Traditional mit der leichten Farbgebung
-                und Ornamenten des Jugendstils. Das erlaubt mir, mit der
-                Formgebung des Körpers zu arbeiten, die Tätowierung aber trotzdem
-                so haltbar wie möglich zu machen.
-              </p>
+                        <div className="text-center">
+                            <img
+                                src={frau}
+                                alt="Tattoo Künstlerin"
+                                className="aboutme-img"
+                                loading="lazy"
+                            />
+                        </div>
+                    </div>
 
-              <p>
-                Am liebsten arbeite ich in Farbe, Black'n Grey ist aber natürlich
-                auch immer möglich.
-              </p>
+                    <hr className="aboutme-divider" />
 
-              <p>
-                Mein anderer Schwerpunkt liegt beim Covern von alten
-                Tätowierungen und Narben. Ich habe in den letzten Jahren in
-                diesen Bereichen viel Erfahrung gesammelt und bin davon
-                überzeugt, dass es immer eine Möglichkeit gibt, alte
-                Tätowierungen oder Narben abzudecken oder zu verschönern.
-              </p>
+                    <div className="aboutme-columns">
 
-              <p>
-                Wenn Ihr unsicher seid, was überhaupt möglich ist oder Euch eine
-                konkrete Idee fehlt, können wir gerne ein unverbindliches
-                Beratungsgespräch vereinbaren.
-              </p>
+                        <div className="aboutme-block">
+                            <span className="aboutme-block-title">Mein Stil</span>
+                            <p>
+                                Ich habe als Allrounderin angefangen und mich inzwischen auf{" "}
+                                <em>Neotraditional</em> spezialisiert – ein Stil, der die klare
+                                Linienführung des Traditional mit der leichten Farbgebung und
+                                Ornamenten des Jugendstils verbindet. Am liebsten arbeite ich in
+                                Farbe, Black'n Grey ist aber natürlich auch möglich.
+                            </p>
+                        </div>
+
+                        <div className="aboutme-block">
+                            <span className="aboutme-block-title">Warum Neotraditional?</span>
+                            <p>
+                                Dieser Stil erlaubt mir, mit der Formgebung des Körpers zu arbeiten
+                                und die Tätowierung trotzdem so haltbar wie möglich zu gestalten –
+                                Ästhetik und Langlebigkeit gehen hier Hand in Hand.
+                            </p>
+                        </div>
+
+                        <div className="aboutme-block">
+                            <span className="aboutme-block-title">Cover-Ups & Narben</span>
+                            <p>
+                                Mein zweiter Schwerpunkt liegt beim Überarbeiten alter Tätowierungen
+                                und beim Abdecken von Narben. Ich habe in diesen Bereichen viel
+                                Erfahrung gesammelt und bin überzeugt: Es gibt immer eine Möglichkeit,
+                                etwas zu verschönern.
+                            </p>
+                        </div>
+
+                        <div className="aboutme-block">
+                            <span className="aboutme-block-title">Unverbindliche Beratung</span>
+                            <p>
+                                Ihr seid unsicher, was möglich ist, oder habt noch keine konkrete
+                                Idee? Meldet euch gerne – wir finden gemeinsam heraus, was zu euch
+                                passt.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <div className="col-md-6 text-center">
-            <img
-              src={frau}
-              alt="Tattoo Künstlerin"
-              className="aboutme-img"
-              loading="lazy"
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/components/Studio/Studio.jsx
+++ src/components/Studio/Studio.jsx
@@ -9,18 +9,17 @@ export default function Studio() {
       <div id="studio" />
       <div className="studio-wrapper">
         <div className="studio-container">
-          <div className="studio-text-col">
-            <img src={studioText} alt="Über mich" className="studio-heading" loading="lazy"/>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua.
-            </p>
-          </div>
           <div className="studio-image-col">
-            <img src={studio} alt="Tattoo Künstlerin" className="studio-img" loading="lazy"/>
+            <img src={studio} alt="Studio Villa bunter Hund" className="studio-img" loading="lazy" />
+          </div>
+          <div className="studio-text-col">
+            <img src={studioText} alt="Studio" className="studio-heading" loading="lazy" />
+            <p className="studio-body">
+              Zusammen mit zwei Kolleg*innen bin ich Hauptverantwortliche im Tattoo Kollektiv <span className="studio-intro-name">Villa bunter Hund, </span>
+              ein Ort für gemeinschaftliches, kreatives Arbeiten in gemütlicher Atmosphäre, ein bisschen versteckt in Tübingen-Lustnau. Nur auf Termin geöffnet, aber jeden ersten Samstag im Monat heißen wir euch bei{" "}
+              <span className="studio-highlight">Tats'n Tea</span>{" "}
+              willkommen: vorbeischauen, kennenlernen, beraten oder tätowieren lassen.
+            </p>
           </div>
         </div>
       </div>
